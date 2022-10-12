30dc3d481971bd9a2124975fee45423dea308c4a
Johnnyfdev <johnnyfreirejh@hotmail.com>
2022-10-12 14:26:29 -0300

    refactor heading component

--- src/components/Heading.stories.tsx
+++ src/components/Heading.stories.tsx
@@ -41,6 +41,12 @@ export const CustomComponent: StoryObj<HeadingProps> = {
             table:{
                 disable:true,
             }
-        }
+        },
+        asChild:{
+            table:{
+                disable:true,
+            }
+        },
+        
     }
 }
